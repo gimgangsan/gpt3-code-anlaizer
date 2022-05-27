b2cc81ad479e4f12d76ff8de1aa4a9ea570b69d7
gimgangsan <gimgansan@gmail.com>
2022-05-27 16:24:36 +0900

    text analizer implemented

---
+++ gpt3/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace gpt3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_textInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // tb_textInput
+            // 
+            this.tb_textInput.Location = new System.Drawing.Point(46, 37);
+            this.tb_textInput.Multiline = true;
+            this.tb_textInput.Name = "tb_textInput";
+            this.tb_textInput.Size = new System.Drawing.Size(327, 213);
+            this.tb_textInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "text input";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "analize text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(390, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "type and frequency of word";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(392, 37);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(314, 213);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "word";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "count";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_textInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_textInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ gpt3/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gpt3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textInput = tb_textInput.Text.ToLower();
+            textAnalizer textAnalizer = new textAnalizer(textInput);
+            foreach(string word in textAnalizer.usedWord)
+            {
+                String[] strArray = new String[] { word, textAnalizer.wordCount[word].ToString() };
+                ListViewItem lvt = new ListViewItem(strArray);
+                listView1.Items.Add(lvt);
+            }
+        }
+    }
+
+    public class textAnalizer
+    {
+        public Dictionary<string, int> wordCount = new Dictionary<string, int> { };
+        public string[] usedWord = new string[] { };
+
+        public textAnalizer(string text)
+        {
+            string[] saperatedWords = getSaperateWord(text);
+
+            // delete overlap
+            HashSet<string> wordSet = new HashSet<string>(saperatedWords);
+            usedWord = wordSet.ToArray<string>();
+
+            foreach(string word in saperatedWords)
+            {
+                countWord(word);
+            }
+        }
+
+        public string[] getSaperateWord(string text)
+        {
+            string[] saperatedWords = text.Split(' ');
+            return saperatedWords;
+        }
+
+        public void countWord(string word)
+        {
+            List<string> foundWord = new List<string>(wordCount.Keys);
+            if (foundWord.Contains(word))
+            {
+                wordCount[word] += 1;
+            }
+            else
+            {
+                wordCount[word] = 1;
+            }
+        }
+    }
+
+    
+}
